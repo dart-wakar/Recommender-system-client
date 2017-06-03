3af3b923d914074c3d54208cf52dda24b3f25ecb
dart-wakar <wakar@whitelogic.in>
2017-06-03 09:35:30 +0530

    Recommended movies component

--- src/app/components/recommended-movies/recommended-movies.component.ts
+++ src/app/components/recommended-movies/recommended-movies.component.ts
@@ -1,7 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,Input,OnInit,OnChanges } from '@angular/core';
+import { RecommendedMoviesService } from '../../services/recommended-movies.service';
 
 @Component({
   selector: 'recommended-movies',
   templateUrl: './recommended-movies.component.html'
 })
-export class RecommendedMoviesComponent  { componentName = 'RecommendedMovies'; }
+export class RecommendedMoviesComponent implements OnInit,OnChanges {
+
+     componentName = 'RecommendedMovies';
+     @Input() inputMovie:any;
+     recommendedMovies:any;
+
+     constructor(private recommendedMoviesService: RecommendedMoviesService) {}
+
+     ngOnInit() {
+         this.getRecommendedMovies();
+     }
+
+     ngOnChanges() {
+         this.getRecommendedMovies();
+     }
+
+     getRecommendedMovies() {
+         this.recommendedMoviesService.getRecommendationsForSingleMovie(this.inputMovie.id)
+            .subscribe(recommendedMovies => {
+                console.log(recommendedMovies);
+                this.recommendedMovies = recommendedMovies;
+            },err => console.log(err));
+     }
+
+}
--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -14,43 +14,11 @@ export class TestComponent implements OnInit  {
 
     constructor(private recommendedMoviesService: RecommendedMoviesService) {}
 
-    /*constructor(private searchService:SearchService,private recommendedMoviesService: RecommendedMoviesService) {
-        var x= this;
-        this.searchService.searchMovies(this.searchKey)
-            .subscribe(movies => {
-                x.searchKey.subscribe(term => this.currentTerm = term);
-                if(this.currentTerm.length != 0) {
-                    x.showSuggestions = true;
-                    console.log(x.showSuggestions);
-                } else {
-                    this.showSuggestions = false;
-                }
-                this.movies = movies;
-                console.log(this.movies)
-            });
-    }*/
-
     ngOnInit() {
         
     }
 
-    /*selectItem(movie:any) {
-        this.key = movie.title;
-        this.showSuggestions = false;
-        this.selectedMovie = movie;
-    }*/
-    
-    getRecommendedMovies() {
-        console.log(this.slctdMovie.id);
-        this.recommendedMoviesService.getRecommendationsForSingleMovie(this.slctdMovie.id)
-            .subscribe(movies => {
-                console.log(movies);
-                this.recommendedMovies = movies;
-            },err => console.log(err));
-    }
-
     onMovieSelect(movie:any) {
         this.slctdMovie = movie;
-        this.getRecommendedMovies();
     }
 }
